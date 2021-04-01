ab8cca436dcd76ec2a66433a2cfc1ffec5f4dedd
sollimann <kristoffer.solberg@cognite.com>
2021-04-01 04:02:19 +0200

    successfully set up server stream to client

--- simulator/src/client.rs
+++ simulator/src/client.rs
@@ -1,1 +1,38 @@
-fn main() {}
+
+pub mod sensors_proto {
+    tonic::include_proto!("sensors");
+}
+
+use sensors_proto::sensors_client::SensorsClient;
+use sensors_proto::{Pose, LaserScan, SensorData, SensorsRequest};
+use tonic::transport::Channel;
+use std::error::Error;
+use tonic::Request;
+
+async fn print_sensor_data(client: &mut SensorsClient<Channel>) -> Result<(), Box<dyn Error>> {
+    let freq = SensorsRequest {
+        stream_frequency: 10
+    };
+
+    let mut stream = client
+        .list_data(Request::new(freq))
+        .await?
+        .into_inner();
+
+    while let Some(data) = stream.message().await? {
+        println!("NOTE = {:?}", data);
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = SensorsClient::connect("http://[::1]:10000").await?;
+
+
+    println!("\n*** SERVER STREAMING ***");
+    print_sensor_data(&mut client).await?;
+
+    Ok(())
+}
