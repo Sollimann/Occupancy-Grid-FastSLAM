4eb1dfd036a8da12bee69f757b24a2f245c3be50
sollimann <kristoffer.solberg@cognite.com>
2021-04-17 00:11:48 +0200

    able to load map into piston window

--- fastslam/src/display_image.rs
+++ fastslam/src/display_image.rs
@@ -1,29 +1,22 @@
+use image::{GenericImageView, Rgb, GenericImage, ImageBuffer};
+use std::thread;
+use imageproc::drawing::draw_cross_mut;
+use std::time::Duration;
+use std::sync::{Mutex, Arc, MutexGuard};
+use piston_window::{PistonWindow, WindowSettings};
 
-fn show_img() {
+
+fn load_map() {
     use imageproc::window::display_image;
     use std::env;
 
-    let image = image::open("maps/map_large.png")
+    let mut image = image::open("maps/map_large.png")
         .expect("No image found at provided path")
         .to_rgb8();
-
     display_image("gt map", &image, 500, 500);
 }
 
-use image::GenericImageView;
 
 fn main() {
-    // Use the open function to load an image from a Path.
-    // `open` returns a `DynamicImage` on success.
-    // let img = image::open("maps/map_large.png").unwrap();
-    //
-    // // The dimensions method returns the images width and height.
-    // println!("dimensions {:?}", img.dimensions());
-    //
-    // // The color method returns the image's `ColorType`.
-    // println!("{:?}", img.color());
-    //
-    // // Write the contents of this image to the Writer in PNG format.
-    // img.save("test.png").unwrap();
-    show_img();
+    load_map();
 }
--- simulator/src/run.rs
+++ simulator/src/run.rs
@@ -1,6 +1,30 @@
 use fastslam::occupancy_grid::{OccupancyGrid};
+use piston_window::*;
+use std::path::Path;
+
 
 
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V4_5;
+
+    let mut window: PistonWindow = WindowSettings::new("Vacuum Robot Simulator", [800, 400])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let map: G2dTexture = Texture::from_path(
+        &mut window.create_texture_context(),
+        Path::new("maps/map_large.png"),
+        Flip::None,
+        &TextureSettings::new()
+    ).unwrap();
+
+    window.set_lazy(true);
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g, _| {
+            clear([1.0; 4], g);
+            image(&map, c.transform, g);
+        });
+    }
 }
