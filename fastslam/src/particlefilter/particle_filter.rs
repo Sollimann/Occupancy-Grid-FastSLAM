d175f2785fd9b6955aa2fb7dc6758b1a1ecbbbe7
sollimann <kristoffer.solberg@cognite.com>
2021-05-02 22:11:56 +0200

    added algorithm steps

--- fastslam/src/particlefilter/particle_filter.rs
+++ fastslam/src/particlefilter/particle_filter.rs
@@ -38,14 +38,42 @@ impl Default for ParticleFilter {
 
 impl ParticleFilter {
 
-    pub fn cycle(&mut self, scan: &Scan, pose: &Pose, gain: &Twist) {
+    /// particles: S_t-1 - the sample set of the previous step
+    /// scan: z_t - the most recent laser scan
+    /// gain: u_t-1 - the most recent gain, applied in the previous step
+    pub fn cycle(&mut self, scan: &Scan, gain: &Twist) {
 
         // This is an iterator-like chain that potentially executes in parallel
+        // we iterate over all particles in the filter and do the following
         self.particles
             .par_iter_mut()
             .for_each(|p: &mut Particle| {
                 p.weight = 2.0
 
+
+                // step 1.)
+                // initial guess of pose x'_ bade on motion model
+
+                // step 2.)
+                // scan-matching using the initial guess x'_t and the latest scan m_t
+                // to compute a pose estimate x*_t
+
+                // step 3.)
+                // sample points around the pose x*_t (using nearest neighbor?)
+
+                // step 4.)
+                // compute new pose x_t drawn from the gaussian approximation of the
+                // improved proposal distribution
+
+                // step 5.)
+                // update the importance weights
+
+                // step 6.)
+                // updating the map according to the drawn pose x_t and the observation z_t
+
+                // step 7.)
+                // compute efficient number of particles and resample based on
+                // computed weights if Neff drops below threshold
 
             });
 
